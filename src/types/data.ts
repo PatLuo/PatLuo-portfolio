7c4ce27e3884d91478c0adccde6e710d79a0f4ab
Pat Luo <patrick.luong615@gmail.com>
2024-07-12 21:35:39 -0400

    add education and remove testimonials

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,105 +1,106 @@
 export interface NavbarLink {
-  label: string;
-  path: string;
+	label: string;
+	path: string;
 }
 
 export interface Navbar {
-  links: NavbarLink[];
+	links: NavbarLink[];
 }
 
 export interface HomeSections {
-  banner: boolean;
-  experience: boolean;
-  project: boolean;
-  education: boolean;
-  testimonial: boolean;
+	banner: boolean;
+	experience: boolean;
+	project: boolean;
+	education: boolean;
+	testimonial: boolean;
 }
 
 export interface Home {
-  sections: HomeSections;
+	sections: HomeSections;
 }
 
 type Visual = {
-  navbar: Navbar;
-  home: Home;
+	navbar: Navbar;
+	home: Home;
 };
 
 export interface PersonalInfo {
-  name: string;
-  title: string;
-  location: string;
-  bio: string;
+	name: string;
+	title: string;
+	location: string;
+	bio: string;
 }
 
 export interface ContactInfo {
-  email: string;
-  phone: string;
-  linkedin: string;
-  twitter: string;
-  github: string;
-  website: string;
+	email: string;
+	phone: string;
+	linkedin: string;
+	twitter: string;
+	github: string;
+	website: string;
 }
 
 export interface Skills {
-  languages: string[];
-  frameworks: string[];
-  databases: string[];
-  tools: string[];
+	languages: string[];
+	frameworks: string[];
+	databases: string[];
+	tools: string[];
 }
 
 export interface Project {
-  title: string;
-  description: string;
-  technologies: string[];
-  live_url: string;
-  code_repo_url: string;
-  cover: string;
+	title: string;
+	description: string;
+	technologies: string[];
+	live_url: string;
+	code_repo_url: string;
+	cover: string;
 }
 
 export interface WorkExperience {
-  id: string;
-  company: string;
-  companyWebsite: string;
-  role: string;
-  startDate: string;
-  endDate: string;
-  technologies: string[];
-  keyResponsibilities: string[];
+	id: string;
+	company: string;
+	companyWebsite: string;
+	role: string;
+	startDate: string;
+	endDate: string;
+	technologies: string[];
+	keyResponsibilities: string[];
 }
 
 export interface Education {
-  id: string;
-  institution: string;
-  degree: string;
-  description: string;
-  startDate: string;
-  endDate: string;
+	id: string;
+	institution: string;
+	degree: string;
+	courses: string;
+	description: string;
+	startDate: string;
+	endDate: string;
 }
 
 export interface Testimonial {
-  id: string;
-  name: string;
-  avatar: string;
-  title: string;
-  company: string;
-  feedback: string;
+	id: string;
+	name: string;
+	avatar: string;
+	title: string;
+	company: string;
+	feedback: string;
 }
 
 export interface Certification {
-  title: string;
-  institution: string;
-  date: string;
+	title: string;
+	institution: string;
+	date: string;
 }
 
 export interface Data {
-  personalInfo: PersonalInfo;
-  contactInfo: ContactInfo;
-  skills: Skills;
-  projects: Project[];
-  workExperience: WorkExperience[];
-  education: Education[];
-  certifications: Certification[];
-  hobbies: string[];
-  testimonials: Testimonial[];
-  visual: Visual;
+	personalInfo: PersonalInfo;
+	contactInfo: ContactInfo;
+	skills: Skills;
+	projects: Project[];
+	workExperience: WorkExperience[];
+	education: Education[];
+	certifications: Certification[];
+	hobbies: string[];
+	testimonials: Testimonial[];
+	visual: Visual;
 }
