896367b997554edf0119576675347b88a9693ee1
Pat Luo <patrick.luong615@gmail.com>
2024-07-12 22:37:15 -0400

    remove unused assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/ui/footer";
 import Navbar from "@/components/ui/navbar";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
@@ -29,7 +28,6 @@ export default function RootLayout({
 					<Navbar />
 					<div className="h-20"></div>
 					{children}
-					<Footer />
 				</ThemeProvider>
 			</body>
 		</html>
