d0f14e0e1cfd4cab9eaadcaa6f7f5f9f0b63ab20
Pat Luo <patrick.luong615@gmail.com>
2024-07-16 04:29:02 -0400

    add default image and blog schema

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
-import default_img from "@/public/default_img.png";
+import default_img from "../../public/assets/default_img.png";
 
 export const sanityClient = createClient({
 	apiVersion: "2023-05-03",
