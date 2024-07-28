5f6e381e7589cca535da83fb1ca1eff5aebe1089
Pat Luo <patrick.luong615@gmail.com>
2024-07-27 23:58:05 -0400

    Remove unused Sanity files

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,9 @@ const nextConfig = {
 	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 	images: {
 		domains: ["cdn.sanity.io"],
+		unoptimized: true,
 	},
+	output: "export",
 };
 
 export default withMDX()(nextConfig);
